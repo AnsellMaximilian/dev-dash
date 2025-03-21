5d8b97f5f18d63cff1e2d85c1478919317c1cc74
Ansell Maximilian <ansellmaximilian@gmail.com>
2025-03-21 09:42:07 +0700

    dev birthday countdown

---
+++ src/components/DevBirthdayCountdown.tsx
@@ -0,0 +1,77 @@
+import Countdown, { CountdownRendererFn } from "react-countdown";
+import moment from "moment";
+import { LiaBirthdayCakeSolid } from "react-icons/lia";
+
+const CountdownCard = ({ value, label }: { value: number; label: string }) => {
+  return (
+    <div className="d-flex flex-column justify-content-center align-items-center">
+      <div
+        className="border border-2 border-primary w-100 fs-5 flex-grow-1 d-flex justify-content-center align-items-center fw-bold"
+        style={{ height: 75 }}
+      >
+        {value}
+      </div>
+      <div className="fw-bold small mt-1">{label}</div>
+    </div>
+  );
+};
+
+const renderer: CountdownRendererFn = ({
+  days,
+  hours,
+  minutes,
+  seconds,
+  completed,
+}) => {
+  if (completed) {
+    return <h1>Happy Birthday!</h1>;
+  } else {
+    return (
+      <div className="row g-1" style={{ width: 300 }}>
+        <div className="col-3">
+          <CountdownCard value={days} label="Days" />
+        </div>
+        <div className="col-3">
+          <CountdownCard value={hours} label="Hours" />
+        </div>
+        <div className="col-3">
+          <CountdownCard value={minutes} label="Minutes" />
+        </div>
+        <div className="col-3">
+          <CountdownCard value={seconds} label="Seconds" />
+        </div>
+      </div>
+    );
+  }
+};
+
+export default function DevBirthdayCountdown({
+  dateJoined,
+}: {
+  dateJoined: string;
+}) {
+  const currentYear = moment().year();
+  const devBirthdayThisYear = moment(dateJoined).year(currentYear);
+
+  const nextDevBirthday = devBirthdayThisYear.isBefore(moment())
+    ? devBirthdayThisYear.add(1, "year")
+    : devBirthdayThisYear;
+  return (
+    <div className="d-flex align-items-start flex-column">
+      <div className="d-flex align-items-center mb-2">
+        <LiaBirthdayCakeSolid size={44} />
+        <div className="ms-1">
+          <div className="fw-bold">Dev Birthday</div>
+          <div
+            style={{
+              marginTop: -6,
+            }}
+          >
+            {devBirthdayThisYear.format("DD MMMM")}
+          </div>
+        </div>
+      </div>
+      <Countdown date={nextDevBirthday.toDate()} renderer={renderer} />
+    </div>
+  );
+}
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -12,6 +12,7 @@ import {
   TileLayoutRepositionEvent,
 } from "@progress/kendo-react-layout";
 import Tile from "../components/dashboard/Tile";
+import DevBirthdayCountdown from "../components/DevBirthdayCountdown";
 
 export default function Dashboard() {
   const [apiKeyDialogOpen, setApiKeyDialogOpen] = useState(false);
@@ -39,68 +40,71 @@ export default function Dashboard() {
         body: (
           <Tile metadata={{ loading: devUser.loading, error: devUser.error }}>
             {devUser.data && (
-              <div className="d-flex h-100" style={{ gap: 16 }}>
-                <div className="d-flex flex-column">
-                  <img
-                    src={devUser.data.profile_image}
-                    alt="Profile"
-                    className="rounded-circle d-block"
-                    style={{
-                      width: 125,
-                      height: 125,
-                    }}
-                  />
-                  <div
-                    className="d-flex justify-content-center mt-3"
-                    style={{ gap: 8 }}
-                  >
-                    <a
-                      href={`https://github.com/${devUser.data.github_username}`}
-                      target="_blank"
-                      className="d-flex align-items-center icon-btn"
+              <div className="d-flex h-100 justify-content-between">
+                <div className="d-flex h-100" style={{ gap: 16 }}>
+                  <div className="d-flex flex-column">
+                    <img
+                      src={devUser.data.profile_image}
+                      alt="Profile"
+                      className="rounded-circle d-block"
+                      style={{
+                        width: 125,
+                        height: 125,
+                      }}
+                    />
+                    <div
+                      className="d-flex justify-content-center mt-3"
+                      style={{ gap: 8 }}
                     >
-                      <FaGithub size={24} color="black" />
-                    </a>
-                    <a
-                      href={`https://x.com/${devUser.data.twitter_username}`}
-                      target="_blank"
-                      className="d-flex align-items-center icon-btn"
-                    >
-                      <SvgIcon
-                        icon={svgIcons.twitterIcon}
-                        color="#1DA1F2"
-                        size="xlarge"
-                      />
-                    </a>
-                    <a
-                      href={devUser.data.website_url}
-                      target="_blank"
-                      className="d-flex align-items-center icon-btn"
-                    >
-                      <SvgIcon
-                        icon={svgIcons.globeOutlineIcon}
-                        color="gray"
-                        size="xlarge"
-                      />
-                    </a>
+                      <a
+                        href={`https://github.com/${devUser.data.github_username}`}
+                        target="_blank"
+                        className="d-flex align-items-center icon-btn"
+                      >
+                        <FaGithub size={24} color="black" />
+                      </a>
+                      <a
+                        href={`https://x.com/${devUser.data.twitter_username}`}
+                        target="_blank"
+                        className="d-flex align-items-center icon-btn"
+                      >
+                        <SvgIcon
+                          icon={svgIcons.twitterIcon}
+                          color="#1DA1F2"
+                          size="xlarge"
+                        />
+                      </a>
+                      <a
+                        href={devUser.data.website_url}
+                        target="_blank"
+                        className="d-flex align-items-center icon-btn"
+                      >
+                        <SvgIcon
+                          icon={svgIcons.globeOutlineIcon}
+                          color="gray"
+                          size="xlarge"
+                        />
+                      </a>
+                    </div>
                   </div>
-                </div>
-                <div className="d-flex h-100" style={{ gap: 24 }}>
-                  <div>
-                    <h2 className="h4 m-0">{devUser.data.name}</h2>
-                    <a
-                      href={`https://dev.to/${devUser.data.username}`}
-                      target="_blank"
-                      className="small"
-                    >
-                      @{devUser.data.username}
-                    </a>
+                  <div className="d-flex h-100" style={{ gap: 24 }}>
+                    <div>
+                      <h2 className="h4 m-0">{devUser.data.name}</h2>
+                      <a
+                        href={`https://dev.to/${devUser.data.username}`}
+                        target="_blank"
+                        className="small"
+                      >
+                        @{devUser.data.username}
+                      </a>
 
-                    <div className="mt-2 small">
-                      {devUser.data.summary || "No summary yet."}
+                      <div className="mt-2 small">
+                        {devUser.data.summary || "No summary yet."}
+                      </div>
                     </div>
                   </div>
                 </div>
+                <DevBirthdayCountdown dateJoined={devUser.data.joined_at} />
               </div>
             )}
           </Tile>
